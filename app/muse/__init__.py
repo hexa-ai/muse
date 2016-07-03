14400c408091a5a4c1d8b1601d60e9be338e1c81
jeremyfromearth <jeremynealbrown@gmail.com>
2016-07-03 15:44:16 -0700

    Added code to test sending modified arrays back to clients

--- app/muse/__init__.py
+++ app/muse/__init__.py
@@ -1,3 +1,4 @@
+from random import random
 from flask import Flask
 from flask import render_template
 from flask_socketio import SocketIO
@@ -7,9 +8,13 @@ app.config.from_object('config')
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode="eventlet")
 
-@socketio.on('update')
-def handle_message(message):
-    print('received update')
+@socketio.on('in')
+def handle_message(json):
+    for i in range(len(json)):
+        for x in range(len(json[i])):
+            json[i][x] = 1 if random() > 0.5 else 0
+    socketio.emit('out', json)
+    
 
 @app.route('/')
 def index():
