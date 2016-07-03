371cca68a69292b45974bf582f7cbfab4fc3a1b5
jeremyfromearth <jeremy.from.earth@gmail.com>
2016-07-02 18:00:58 -0700

    Added static js file test and socketio integration

--- app/muse/__init__.py
+++ app/muse/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
 from flask import render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode="eventlet")
+
+@socketio.on('message')
+def handle_message(message):
+        print('received message: ' + message)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    socketio.run(app, debug=app.config['DEBUG'])
