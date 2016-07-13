d2041f0fe4ac597ba82ae92accbf083d841014d9
jessebelcourt <jessebelcourt@gmail.com>
2016-07-13 16:57:45 -0700

    Adds function to indicate which index to strart reading from buffer.

--- app/muse/static/js/playground/music_counter.js
+++ app/muse/static/js/playground/music_counter.js
@@ -3,17 +3,30 @@ var BUFFER_SIZE = 8192;
 var SAMPLE_RATE = 44100;
 
 function StepSequencer() {
-	var ctx;
     var tempo = 30.0;
     var steps = 16;
     var step = 0;
     var sources = {};
     var loader = new AudioSampleLoader();
     var sampleBuffers;
+    var kickDrumBuffer;
+    var kickDrumBufferLength;
     var toggles = [
     	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     	[1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1] //kickdrum
     ];
+
+    var getBufferIndex = function(sampleBufferLength, cycle) {
+        var sampleBufferLength = sampleBufferLength; //31311 for testing purposes
+
+        remainder = (cycle * BUFFER_SIZE) % sampleBufferLength;
+        chunkToPlay = Math.floor(remainder / BUFFER_SIZE);
+        console.log("Remainder: "+remainder, "Chunk: "+chunkToPlay, "Cycle: "+cycle, chunkToPlay * BUFFER_SIZE);
+        return chunkToPlay * BUFFER_SIZE;
+            //really strange skip from chunk 2 straight to 0 every so often. Can't figure out why....Driving me crazy. 
+            //Happens when remainder is in increments of 550, so 550 1100 etc. 
+    }
+
     loader.ctx = engine.ctx;
 
 	loader.src = [
@@ -27,7 +40,9 @@ function StepSequencer() {
 
 	loader.onload = function() {
 		sampleBuffers = loader.response;
-		// console.log(sampleBuffers);
+		kickDrumBuffer = sampleBuffers[0].getChannelData(0); // Needs to be more generic, works for testing. 
+        kickDrumBufferLength = kickDrumBuffer.length; //Same as line above
+
 	}
 
 loader.send();
@@ -38,24 +53,22 @@ loader.send();
     	name : 'Step Sequencer',
         process : function(channels, cycle) {
             var stepInterval = Math.round(((SAMPLE_RATE * 60.0) / (tempo * steps))); //5513
-            
             if(channels.length) {
-                var bufferSize = channels[0].length;
-                var startSampleIndex = bufferSize * cycle;
-                var tempBuffer;
+                var startSampleIndex = getBufferIndex(kickDrumBufferLength, cycle);
                 var output = channels[0];
-                var tempBuffer = sampleBuffers[0].getChannelData(0);
                 for(var i = 0; i < channels[0].length; i++) {
                     if((startSampleIndex + i) % stepInterval == 0) {
-                    	console.log(step);
                     	if(step == 15) {
                     		step = 0;
                     	} else {
                     		step++;
                     	}
-                    	// output[i] = tempBuffer[startSampleIndex+i];
                     }
-                    output[i] = tempBuffer[startSampleIndex+i];
+                    if((startSampleIndex+i)<=kickDrumBufferLength-1) {
+                        output[i] = kickDrumBuffer[startSampleIndex + i];
+                    } else {
+                        continue;
+                    }
                 }
             }
         },
@@ -94,7 +107,7 @@ function AudioEngine() {
         
         if(channels.length >= 1) {            
             for(var i = 0; i < nodes.length; i++) {
-                nodes[i].process(channels, cycle, ctx);
+                nodes[i].process(channels, cycle);
             }
         }
         
@@ -114,10 +127,8 @@ function AudioEngine() {
         	// nodes.push(node);
         	if(node.hasOwnProperty('name')) {
         		if(node.name == 'Step Sequencer') {
-
         			node.ctx = ctx;
         			nodes.push(node);
-        			console.log(node);
         		}
         	} else {
         		nodes.push(node);
