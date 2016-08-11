5ae47531bd6b67a3e6af9ed28181a1d7a102422b
jeremyfromearth <jeremy.from.earth@gmail.com>
2016-08-11 16:04:57 -0700

    Front end is now sending data to server to be saved in SQL db

--- app/muse/models.py
+++ app/muse/models.py
@@ -1,5 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy, Model
 db = SQLAlchemy()
 
+class Sequence(db.Model):
+    __tablename__ = 'Sequence'
+    m = db.Column(db.Integer)
+    n = db.Column(db.Integer)
+    data = db.Column(db.Text)
+    id = db.Column(db.Integer, primary_key=True)
+
+    def __init__(self, json):
+        self.m = json['shape'][0]
+        self.n = json['shape'][1]
+        data = json['data']
+
 class Composition(db.Model):
     id = db.Column(db.Integer, primary_key=True)
