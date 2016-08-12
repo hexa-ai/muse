e48940e65a610be7e13a40301bc00ef8e5cb7664
jeremyfromearth <jeremy.from.earth@gmail.com>
2016-08-12 16:47:39 -0700

    Added relationship between compositions and sequences models

--- app/muse/models.py
+++ app/muse/models.py
@@ -1,6 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy, Model
 db = SQLAlchemy()
 
+class Composition(db.Model):
+    __tablename__ = 'Composition'
+    id = db.Column(db.Integer, primary_key=True)
+    sequences = db.relationship('Sequence', backref='composition', lazy='dynamic')
+
 class Sequence(db.Model):
     __tablename__ = 'Sequence'
     m = db.Column(db.Integer)
@@ -8,6 +13,7 @@ class Sequence(db.Model):
     data = db.Column(db.Text)
     instrument = db.Column(db.Text)
     id = db.Column(db.Integer, primary_key=True)
+    composition_id = db.Column(db.Integer, db.ForeignKey('Composition.id'))
 
     def __init__(self, json):
         self.m = json['shape'][0]
@@ -18,6 +24,4 @@ class Sequence(db.Model):
     def __repr__(self):
         return 'Sequence {} {} {} {}'.format(self.id, self.instrument, self.m, self.n)
 
-class Composition(db.Model):
-    __tablename__ = 'Composition'
-    id = db.Column(db.Integer, primary_key=True)
+
