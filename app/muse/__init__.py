63c7f0ec570b09b938534269a04281113b891b02
jeremyfromearth <jeremynealbrown@gmail.com>
2016-08-12 16:47:39 -0700

    Added relationship between compositions and sequences models

--- app/muse/__init__.py
+++ app/muse/__init__.py
@@ -1,29 +1,30 @@
 import json
 from flask import Flask, request
 from flask import render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send, emit
 from muse.models import db, Sequence
 
 app = Flask(__name__)
 app.config.from_object('config')
-
 socketio = SocketIO(app, async_mode="eventlet")
 
 db.init_app(app)
 with app.app_context():
+    meta = db.metadata
+    '''
+    # clear the database
+    for table in reversed(meta.sorted_tables):
+        print('Clear table %s' % table)
+        db.session.execute(table.delete())
+    db.session.commit()
+    '''
     db.create_all()
 
 @socketio.on('connect')
 def handle_connect():
-    pass
-
-@socketio.on('disconnect')
-def handle_disconnect():
-    pass
-
-@socketio.on('sequence')
-def handle_message(data):
-    pass 
+    # using send() sends a message back to a single client
+    # use socketio.send() to send to all clients
+    send('init_composition')
 
 @socketio.on('save_sequence')
 def handle_save_sequence(data):
@@ -36,6 +37,10 @@ def handle_get_sequences():
     sequences = Sequence.query.all()
     for sequence in sequences:
         print(sequence)
+
+@socketio.on('sequence_update')
+def handle_sequence_update(data):
+    pass
 
 @app.route('/')
 def index():
