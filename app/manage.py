9a760fcee833c002cf486751fad552f174e3d728
jeremyfromearth <jeremynealbrown@gmail.com>
2016-08-14 13:21:41 -0700

    Added method to destory the database file, changed name of destroy method to clear, which just clears the tables of the database rather than destroying the actual file

--- app/manage.py
+++ app/manage.py
@@ -1,17 +1,28 @@
+import os
+import os.path
+import config
 from muse import app, db
 
-cmd = input('Enter a command -> create | delete: ')
+cmd = input('Enter a command -> create | clear | destroy: ')
 
 if cmd == 'create':
     with app.app_context():
         print('Creating database')
         db.create_all()
 
-if cmd == 'delete':
+if cmd == 'clear':
     with app.app_context():
-        print('Deleting database')
+        print('Clearing database')
         meta = db.metadata
         for table in reversed(meta.sorted_tables):
             print('Clearing table %s' % table)
             db.session.execute(table.delete())
         db.session.commit()
+
+if cmd == 'destroy':
+    print('Destroying database file')
+    if os.path.exists(config.DATABASE_FILENAME):
+        os.remove(config.DATABASE_FILENAME)
+        print('Database file removed')
+        
+    
