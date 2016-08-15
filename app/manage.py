94b6fecb4d308fad1f9eb5c2b49637db3652832b
jeremyfromearth <jeremynealbrown@gmail.com>
2016-08-15 12:02:24 -0700

    Added type property to instruments, removed initial state properties

--- app/manage.py
+++ app/manage.py
@@ -10,26 +10,19 @@ if cmd == 'seed-instruments':
         mongo.db.instruments.remove()
         mongo.db.instruments.insert([{
             'name' : 'beatbox',
+            'type' : 'sequencer',
             'voices' : [{
                     'name' : 'Kick',
                     'file' : './static/media/sound/Kick05-Longer.wav',
-                    'toggles' : [0 for x in range(32)],
-                    'enabled' : True
                 }, {
                     'name' : 'Snare',
                     'file' : './static/media/sound/Snare04-Hi-Simmons1.wav',
-                    'toggles' : [0 for x in range(32)],
-                    'enabled' : True
                 }, {
                     'name' : 'Hi-hat',
                     'file' : './static/media/sound/CH.WAV',
-                    'toggles' : [0 for x in range(32)],
-                    'enabled' : True
                 }, {
                     'name' : 'Clap',
                     'file' : './static/media/sound/CP.WAV',
-                    'toggles' : [0 for x in range(32)],
-                    'enabled' : True
                 }
             ]
         }])
