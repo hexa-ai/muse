37a8f3544f85fa6766b62b59b72eedf3618dc543
jeremyfromearth <jeremynealbrown@gmail.com>
2016-08-14 20:48:19 -0700

    Updated seed-instruments to supply database with default set of instruments

--- app/manage.py
+++ app/manage.py
@@ -3,11 +3,33 @@ import os.path
 import config
 from muse import app, mongo
 
-cmd = input('Enter a command -> seed-instruments: ')
+cmd = input('Enter a command -> seed-instruments | clear-collections: ')
 
 if cmd == 'seed-instruments':
     with app.app_context():
-        instruments = ['beats', 'bass', 'synth']
         mongo.db.instruments.remove()
-        for instrument in instruments:
-            mongo.db.instruments.insert_one({'type' : instrument})
+        mongo.db.instruments.insert([{
+            'name' : 'beatbox',
+            'voices' : [{
+                    'name' : 'Kick',
+                    'file' : './static/media/sound/Kick05-Longer.wav',
+                    'toggles' : [0 for x in range(32)],
+                    'enabled' : True
+                }, {
+                    'name' : 'Snare',
+                    'file' : './static/media/sound/Snare04-Hi-Simmons1.wav',
+                    'toggles' : [0 for x in range(32)],
+                    'enabled' : True
+                }, {
+                    'name' : 'Hi-hat',
+                    'file' : './static/media/sound/CH.WAV',
+                    'toggles' : [0 for x in range(32)],
+                    'enabled' : True
+                }, {
+                    'name' : 'Clap',
+                    'file' : './static/media/sound/CP.WAV',
+                    'toggles' : [0 for x in range(32)],
+                    'enabled' : True
+                }
+            ]
+        }])
