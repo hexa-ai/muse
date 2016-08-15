016034eee3554a915e35e170051048bb604fcf6c
jeremyfromearth <jeremy.from.earth@gmail.com>
2016-08-14 17:16:52 -0700

    Refactored app to use Mongo

--- app/muse/__init__.py
+++ app/muse/__init__.py
@@ -2,52 +2,45 @@ import json
 from flask import Flask, request
 from flask import render_template
 from flask_socketio import SocketIO, send, emit
-from muse.models import *
+from flask.ext.pymongo import PyMongo, ObjectId
 
 app = Flask(__name__)
 app.config.from_object('config')
 socketio = SocketIO(app, async_mode="eventlet")
-db.init_app(app)
+mongo = PyMongo(app)
 
 @socketio.on('connect')
 def handle_connect():
     # using send() sends a message back to a single client
     # use socketio.send() to send to all clients
     # create a new composition for this session
-    composition = Composition()
-    db.session.add(composition)
-    db.session.commit()
-
-    # emit a message containing this sessions composition id
-    emit('composition_init', {'composition_id' : composition.id})
+    composition_id = str(mongo.db.compositions.insert_one({}).inserted_id)
+    emit('composition_init', {'composition_id' : composition_id})
 
 @socketio.on('save_composition')
 def handle_save_composition(data):
-    d = json.loads(data)
-    if 'composition_id' in d:
-        print(d['composition_id'])
-        composition = Composition.query.filter_by(id=d['composition_id']).first()
+    compostion_data = json.loads(data)
+    if 'composition_id' in compostion_data:
+        composition_id = ObjectId(d['composition_id'])
+        composition = mongo.db.compositions.find_one({'_id' : composition_id})
         if composition is not None:
             if 'sequences' in d:
                 for sequence in d['sequences']:
-                    instrument = sequence['instrument']
-                    shape = sequence['shape']
-                    toggles = sequence['toggles']
-                    has_seq = False
-                    for seq in composition.sequences.all():
-                        if seq.instrument == instrument:
-                            has_seq = True
-                            print('Sequence for {} exists'.format(instrument))
-                    if not has_seq:
-                        print('Creating new sequence')
-                        db.session.add(Sequence(shape[0], shape[1], toggles, instrument, composition))
-                        db.session.commit()
-
+                    pass
+                      
 @socketio.on('get_sequences')
 def handle_get_sequences():
+    '''
     sequences = Sequence.query.all()
     for sequence in sequences:
         print(sequence)
+    '''
+    pass
+
+@socketio.on('request_new_sequence')
+def handle_get_new_sequence():
+    sequence_id = str(mongo.db.sequences.insert_one({}).insert_id)
+    emit('response_new_sequence', {'sequence_id' : sequence_id })
 
 @socketio.on('sequence_update')
 def handle_sequence_update(data):
