41a1b02b079c521ec2ee9356732dd201271d1056
jeremyfromearth <jeremynealbrown@gmail.com>
2016-08-17 17:37:05 -0700

    Added back the functionality for highlighting the current step in the sequencer

--- app/muse/static/js/sequencer-ui.js
+++ app/muse/static/js/sequencer-ui.js
@@ -1,12 +1,12 @@
 function SequencerUI(container, sequencer, sequenceName, voices) {
-    var toggles = {};
+    var toggles = [];
     var div = document.createElement('div');
     container.appendChild(div);
     for(var voice in voices) {
+        toggles.push([]);
         var voiceName = voices[voice]['name'];
         var voiceId = voices[voice]['voiceId'];
         var toggleDiv = document.createElement('div');
-        toggles[voiceName] = Array(sequencer.getSteps()).fill(0);
         var labelContainer = document.createElement("div");
         var label = document.createElement("p");
         var toggleDiv = document.createElement("div");
@@ -14,7 +14,7 @@ function SequencerUI(container, sequencer, sequenceName, voices) {
         labelContainer.appendChild(label);
         labelContainer.appendChild(toggleDiv);
         container.appendChild(labelContainer);
-        for(var i = 0; i < toggles[voiceName].length; i++) {
+        for(var i = 0; i < sequencer.getSteps(); i++) {
             var toggle = document.createElement("input");
             toggle.type = "checkbox";
             toggle.voiceId = voiceId;
@@ -25,6 +25,17 @@ function SequencerUI(container, sequencer, sequenceName, voices) {
                     toggle.voiceId, toggle.index, toggle.checked);
             });               
             toggleDiv.appendChild(toggle);
+            toggles[toggles.length-1].push(toggle);
+        }
+    } 
+
+    return {
+        setStep : function(step) {
+            for(var i = 0; i < toggles.length; i++) {
+                for(var j = 0; j < toggles[i].length; j++) {
+                   toggles[i][j].style.boxShadow = (j == step) ? "0px 1px 0px blue" : "0px 0px 0px";
+                }
+            }
         }
     }
 }
