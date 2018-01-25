3405889972390c0b0f1a25095aff1119ec418a2f
jeremyfromearth <jeremynealbrown@gmail.com>
2018-01-25 12:44:41 -0800

    Fixed a couple of small issues, removed unused ui elements

--- demo/js/muse.js
+++ demo/js/muse.js
@@ -9,11 +9,10 @@ window.addEventListener('load', function() {
   engine.connect(sequencer);
 
   // Create a new sample bank
-  var data = instrument_data[0];
   var sampleBank = new SampleBank(engine.ctx);
-  sampleBank.loadAll(data['voices'], function success() {
-    var voices = data['voices'];
-    var sequenceName = data['name'];
+  sampleBank.loadAll(beatbox['voices'], function success() {
+    var voices = beatbox['voices'];
+    var sequenceName = beatbox['name'];
     sequenceId++;
 
   // Add voices to the sequencer
@@ -29,109 +28,15 @@ window.addEventListener('load', function() {
     }
 
     // Create the new sequencer UI 
-    var ui = new SequencerUI(document.getElementById('sequencer'), sequencer, sequenceName, voices);
+    sequencerUIs.push(
+      new SequencerUI(
+        document.getElementById('sequencer'), 
+        sequencer, sequenceName, voices)
+    );
   });
-
-  // ---------------------------------------------------------------- 
-  /*
-  // SocketIO Handlers
-  var socket = io.connect('http://' + document.domain + ':' + location.port);
-  socket.on('connect', function() {
-      console.log('Connected');
-  });
-
-  socket.on('composition_init', function(data) {
-      compositionId = compositionId || data['composition_id']; 
-  });
-
-  setInterval(function() {
-      for(var i = 0; i < sequencerUIs.length; i++) {
-          if(sequencerUIs[i].aiToggle.checked) {
-              console.log(compositionId);
-              socket.emit('request_ai_sequence', {composition_id: compositionId});
-          }
-      } 
-  }, 1000);
-
-  socket.on('response_new_sequence', function(data) {
-    // Create a new sample bank
-      var sampleBank = new SampleBank(engine.ctx);
-
-    // Load the set of samples
-    // Add a callback to handle the successful loading of all samples
-      sampleBank.loadAll(data['voices'], 
-          function success() {
-              var voices = data['voices'];
-              var sequenceId = data['_id']['$oid'];
-              var sequenceName = data['name'];
-
-            // Add voices to the sequencer
-            // Use a special voice-id property as a key for the voice
-            // This id is a the name of the voice + the sequence id
-              var voiceIds = [];
-              for (var voice in voices) {
-                  var voiceName = voices[voice]['name'];
-                  var voiceId = voiceName + '-' + sequenceId;
-                  voices[voice]['voiceId'] = voiceId;
-                  voiceIds.push(voiceId);
-                  sequencer.addVoice(voiceId, voiceName, sampleBank.samples[voiceName])
-              }
-
-            // Create the new sequencer UI 
-              var ui = new SequencerUI(document.getElementById('sequencer'), sequencer, sequenceName, voices);
-
-  // Add listeners for save
-              ui.saveButton.addEventListener('click', function(event) {
-                  var toggleData = "";
-                  var voices = sequencer.getVoices(voiceIds);
-                  for(var i in voices) {
-                      var toggles = voices[i]['toggles']
-                      if(toggles) {
-                          for(var j in toggles) {
-                              toggleData += toggles[j];
-                          }
-                      }
-                  }
-
-                  socket.emit('sequence_data_update', {sequence_id : sequenceId, data : toggleData})
-              });
-
-              ui.aiToggle.addEventListener('click', function(event) {
-                  socket.emit('sequence_ai_update', {sequence_id : sequenceId, ai : ui.aiToggle.checked});
-              });
-
-  // Add listeners for name change
-              ui.nameInput.addEventListener('blur', function(event) {
-                  var name = event.target.value;
-                  if(name != '') {
-                      socket.emit('sequence_name_update', {sequence_id : sequenceId, name : event.target.value})
-                  }
-              });
-
-  // Add to list of sequencer uis
-              sequencerUIs.push(ui);
-          }, 
-          function error() {
-              console.log('Error loading sample bank');                         
-          }
-      ); 
-  });
-  */
 
   // ---------------------------------------------------------------- 
   // UI Handlers
-  var instrumentSelect = document.getElementById('instrument-select');
-  var createInstrumentButton = document.getElementById('create-instrument-btn');
-
-  createInstrumentButton.addEventListener('click', function(event) {
-    /*
-        socket.emit('request_new_sequence', {
-            'composition_id' : compositionId, 
-            'instrument' : instrumentSelect.value
-        });
-        */
-  });
-
   var playButton = document.getElementById("play-stop-btn");
   playButton.addEventListener("click", function(event) {
     switch(engine.getState()) {
@@ -144,11 +49,6 @@ window.addEventListener('load', function() {
         playButton.value = 'Stop';
         break;
     }
-  });
-
-  var updateButton = document.getElementById('update-models-btn');
-  updateButton.addEventListener('click', function(event) {
-    //socket.emit('update_models');
   });
 
   var tempoSlider = document.getElementById("tempo-slider");
@@ -183,49 +83,13 @@ window.addEventListener('load', function() {
 });
 // -----------------------------------------------------------
 // Data
-var instrument_data = [
-    {
-      "name" : "beatbox",
-      "type" : "percussion",
-      "voices" : [
-        {"name" : "Kick", "file" : "./sound/Kick05-Longer.wav"}, 
-        {"name" : "Snare", "file" : "./sound/Snare04-Hi-Simmons1.wav"}, 
-        {"name" : "Hi-hat", "file" : "./sound/CH.WAV"}, 
-        {"name" : "Clap", "file" : "./sound/CP.WAV"}
-      ]
-    }, {
-    "name" : "saw-bass", 
-    "type" : "bass", 
-    "voices" : [
-        {"name" : "C", "file" : "./sound/saw-bass-c.wav"},
-        {"name" : "C#","file" : "./sound/saw-bass-c-sharp.wav"},
-        {"name" : "D", "file" : "./sound/saw-bass-d.wav"},
-        {"name" : "D#","file" : "./sound/saw-bass-d-sharp.wav"},
-        {"name" : "E","file" : "./sound/saw-bass-e.wav"},{
-        "name" : "F","file" : "./sound/saw-bass-f.wav"},
-        {"name" : "F#","file" : "./sound/saw-bass-f-sharp.wav"},
-        {"name" : "G", "file" : "./sound/saw-bass-g.wav"},
-        {"name" : "G#", "file" : "./sound/saw-bass-g-sharp.wav"},
-        {"name" : "A", "file" : "./sound/saw-bass-a.wav"},
-        {"name" : "A#", "file" : "./sound/saw-bass-a-sharp.wav"},
-        {"name" : "B", "file" : "./sound/saw-bass-B.wav"}
-      ]
-    },{
-      "name" : "synth-flute", 
-      "type" : "flute", 
-      "voices" : [
-        {"name" : "C", "file" : "./sound/synth-flute-c.wav"},
-        {"name" : "C#", "file" : "./sound/synth-flute-c-sharp.wav"},
-        {"name" : "D", "file" : "./sound/synth-flute-d.wav"},
-        {"name" : "D#", "file" : "./sound/synth-flute-d-sharp.wav"},
-        {"name" : "E", "file" : "./sound/synth-flute-e.wav"},
-        {"name" : "F", "file" : "./sound/synth-flute-f.wav"},
-        {"name" : "F#", "file" : "./sound/synth-flute-f-sharp.wav"},
-        {"name" : "G","file" : "./sound/synth-flute-g.wav"},
-        {"name" : "G#","file" : "./sound/synth-flute-g-sharp.wav"},
-        {"name" : "A","file" : "./sound/synth-flute-a.wav"},
-        {"name" : "A#","file" : "./sound/synth-flute-a-sharp.wav"},
-        {"name" : "B", "file" : "./sound/synth-flute-B.wav"}
-      ]
-    }
-  ];
+var beatbox = {
+  "name" : "beatbox",
+  "type" : "percussion",
+  "voices" : [
+    {"name" : "Kick", "file" : "./sound/Kick05-Longer.wav"}, 
+    {"name" : "Snare", "file" : "./sound/Snare04-Hi-Simmons1.wav"}, 
+    {"name" : "Hi-hat", "file" : "./sound/CH.WAV"}, 
+    {"name" : "Clap", "file" : "./sound/CP.WAV"}
+  ]
+};
--- demo/js/sequencer-ui.js
+++ demo/js/sequencer-ui.js
@@ -1,63 +1,42 @@
 function SequencerUI(container, sequencer, sequenceName, voices) {
-    var toggles = [];
-    var nameInput = document.createElement('input');
-    nameInput.type = 'text';
-    nameInput.value = sequenceName;
-    container.appendChild(nameInput);
+  var toggles = [];
+  var div = document.createElement('div');
+  container.appendChild(div);
 
-    var div = document.createElement('div');
-    container.appendChild(div);
-
-    for(var voice in voices) {
-        toggles.push([]);
-        var voiceName = voices[voice]['name'];
-        var voiceId = voices[voice]['voiceId'];
-        var toggleDiv = document.createElement('div');
-        var labelContainer = document.createElement("div");
-        var label = document.createElement("p");
-        var toggleDiv = document.createElement("div");
-        label.innerHTML = voiceName;
-        labelContainer.appendChild(label);
-        labelContainer.appendChild(toggleDiv);
-        container.appendChild(labelContainer);
-        for(var i = 0; i < sequencer.getSteps(); i++) {
-            var toggle = document.createElement("input");
-            toggle.type = "checkbox";
-            toggle.voiceId = voiceId;
-            toggle.index = i;
-            toggle.addEventListener('change', function(event) {
-                var toggle = event.target;
-                sequencer.enableVoiceAtStep(
-                    toggle.voiceId, toggle.index, toggle.checked);
-            });               
-            toggleDiv.appendChild(toggle);
-            toggles[toggles.length-1].push(toggle);
-        }
-    } 
-
-    var saveBtn = document.createElement('input');
-    saveBtn.type = 'button';
-    saveBtn.value = 'Save';
-    container.appendChild(saveBtn);
-
-    var aiLabel = document.createElement('div');
-    aiLabel.innerHTML = 'Enable AI';
-
-    var aiToggle = document.createElement('input');
-    aiToggle.type = 'checkbox';
-    aiLabel.appendChild(aiToggle);
-    container.appendChild(aiLabel);
-
-    return {
-        aiToggle : aiToggle,
-        saveButton : saveBtn,
-        nameInput : nameInput,
-        setStep : function(step) {
-            for(var i = 0; i < toggles.length; i++) {
-                for(var j = 0; j < toggles[i].length; j++) {
-                   toggles[i][j].style.boxShadow = (j == step) ? "0px 1px 0px blue" : "0px 0px 0px";
-                }
-            }
-        }
+  for(var voice in voices) {
+    toggles.push([]);
+    var voiceName = voices[voice]['name'];
+    var voiceId = voices[voice]['voiceId'];
+    var toggleDiv = document.createElement('div');
+    var labelContainer = document.createElement("div");
+    var label = document.createElement("p");
+    var toggleDiv = document.createElement("div");
+    label.innerHTML = voiceName;
+    labelContainer.appendChild(label);
+    labelContainer.appendChild(toggleDiv);
+    container.appendChild(labelContainer);
+    for(var i = 0; i < sequencer.getSteps(); i++) {
+      var toggle = document.createElement("input");
+      toggle.type = "checkbox";
+      toggle.voiceId = voiceId;
+      toggle.index = i;
+      toggle.addEventListener('change', function(event) {
+        var toggle = event.target;
+        sequencer.enableVoiceAtStep(
+          toggle.voiceId, toggle.index, toggle.checked);
+      });               
+      toggleDiv.appendChild(toggle);
+      toggles[toggles.length-1].push(toggle);
     }
+  } 
+
+  return {
+    setStep : function(step) {
+      for(var i = 0; i < toggles.length; i++) {
+        for(var j = 0; j < toggles[i].length; j++) {
+          toggles[i][j].style.boxShadow = (j == step) ? "0px 1px 0px blue" : "0px 0px 0px";
+        }
+      }
+    }
+  }
 }
