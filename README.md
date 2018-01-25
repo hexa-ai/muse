9ba6771e3cfb7ef401e2b0f1365f8a1be3d759d8
jeremyfromearth <jeremynealbrown@gmail.com>
2018-01-24 22:58:23 -0800

    Audio engine is now working in demo, still need to make a few more updates to clean it up

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 
 A JavaScript WebAudio based engine
 
+Start a python http server from the root dir in order to serve the audio files:
+```
+python -m http.server
+```
+
+Goto: http://localhost:8000
+
--- demo/js/muse.js
+++ demo/js/muse.js
@@ -1,100 +1,125 @@
-// ---------------------------------------------------------------- 
-// Setup the Audio Engine
-var engine = AudioEngine();
-var sequencer = StepSequencer();
-var sequencerUIs = [];
-var compositionId = 0;
-engine.connect(sequencer);
+window.addEventListener('load', function() {
+  // ---------------------------------------------------------------- 
+  // Setup the Audio Engine
+  var engine = AudioEngine();
+  var sequencer = StepSequencer();
+  var sequencerUIs = [];
+  var compositionId = 0;
+  var sequenceId = 0;
+  engine.connect(sequencer);
 
-// ---------------------------------------------------------------- 
-/*
-// SocketIO Handlers
-var socket = io.connect('http://' + document.domain + ':' + location.port);
-socket.on('connect', function() {
-    console.log('Connected');
-});
-
-socket.on('composition_init', function(data) {
-    compositionId = compositionId || data['composition_id']; 
-});
-
-setInterval(function() {
-    for(var i = 0; i < sequencerUIs.length; i++) {
-        if(sequencerUIs[i].aiToggle.checked) {
-            console.log(compositionId);
-            socket.emit('request_ai_sequence', {composition_id: compositionId});
-        }
-    } 
-}, 1000);
-
-socket.on('response_new_sequence', function(data) {
   // Create a new sample bank
-    var sampleBank = new SampleBank(engine.ctx);
+  var data = instrument_data[0];
+  var sampleBank = new SampleBank(engine.ctx);
+  sampleBank.loadAll(data['voices'], function success() {
+    var voices = data['voices'];
+    var sequenceName = data['name'];
+    sequenceId++;
 
-  // Load the set of samples
-  // Add a callback to handle the successful loading of all samples
-    sampleBank.loadAll(data['voices'], 
-        function success() {
-            var voices = data['voices'];
-            var sequenceId = data['_id']['$oid'];
-            var sequenceName = data['name'];
+  // Add voices to the sequencer
+  // Use a special voice-id property as a key for the voice
+  // This id is a the name of the voice + the sequence id
+    var voiceIds = [];
+    for (var voice in voices) {
+      var voiceName = voices[voice]['name'];
+      var voiceId = voiceName + '-' + sequenceId;
+      voices[voice]['voiceId'] = voiceId;
+      voiceIds.push(voiceId);
+      sequencer.addVoice(voiceId, voiceName, sampleBank.samples[voiceName])
+    }
 
-          // Add voices to the sequencer
-          // Use a special voice-id property as a key for the voice
-          // This id is a the name of the voice + the sequence id
-            var voiceIds = [];
-            for (var voice in voices) {
-                var voiceName = voices[voice]['name'];
-                var voiceId = voiceName + '-' + sequenceId;
-                voices[voice]['voiceId'] = voiceId;
-                voiceIds.push(voiceId);
-                sequencer.addVoice(voiceId, voiceName, sampleBank.samples[voiceName])
-            }
+    // Create the new sequencer UI 
+    var ui = new SequencerUI(document.getElementById('sequencer'), sequencer, sequenceName, voices);
+  });
 
-          // Create the new sequencer UI 
-            var ui = new SequencerUI(document.getElementById('sequencer'), sequencer, sequenceName, voices);
+  // ---------------------------------------------------------------- 
+  /*
+  // SocketIO Handlers
+  var socket = io.connect('http://' + document.domain + ':' + location.port);
+  socket.on('connect', function() {
+      console.log('Connected');
+  });
 
-// Add listeners for save
-            ui.saveButton.addEventListener('click', function(event) {
-                var toggleData = "";
-                var voices = sequencer.getVoices(voiceIds);
-                for(var i in voices) {
-                    var toggles = voices[i]['toggles']
-                    if(toggles) {
-                        for(var j in toggles) {
-                            toggleData += toggles[j];
-                        }
-                    }
-                }
+  socket.on('composition_init', function(data) {
+      compositionId = compositionId || data['composition_id']; 
+  });
 
-                socket.emit('sequence_data_update', {sequence_id : sequenceId, data : toggleData})
-            });
+  setInterval(function() {
+      for(var i = 0; i < sequencerUIs.length; i++) {
+          if(sequencerUIs[i].aiToggle.checked) {
+              console.log(compositionId);
+              socket.emit('request_ai_sequence', {composition_id: compositionId});
+          }
+      } 
+  }, 1000);
 
-            ui.aiToggle.addEventListener('click', function(event) {
-                socket.emit('sequence_ai_update', {sequence_id : sequenceId, ai : ui.aiToggle.checked});
-            });
+  socket.on('response_new_sequence', function(data) {
+    // Create a new sample bank
+      var sampleBank = new SampleBank(engine.ctx);
 
-// Add listeners for name change
-            ui.nameInput.addEventListener('blur', function(event) {
-                var name = event.target.value;
-                if(name != '') {
-                    socket.emit('sequence_name_update', {sequence_id : sequenceId, name : event.target.value})
-                }
-            });
+    // Load the set of samples
+    // Add a callback to handle the successful loading of all samples
+      sampleBank.loadAll(data['voices'], 
+          function success() {
+              var voices = data['voices'];
+              var sequenceId = data['_id']['$oid'];
+              var sequenceName = data['name'];
 
-// Add to list of sequencer uis
-            sequencerUIs.push(ui);
-        }, 
-        function error() {
-            console.log('Error loading sample bank');                         
-        }
-    ); 
-});
-*/
+            // Add voices to the sequencer
+            // Use a special voice-id property as a key for the voice
+            // This id is a the name of the voice + the sequence id
+              var voiceIds = [];
+              for (var voice in voices) {
+                  var voiceName = voices[voice]['name'];
+                  var voiceId = voiceName + '-' + sequenceId;
+                  voices[voice]['voiceId'] = voiceId;
+                  voiceIds.push(voiceId);
+                  sequencer.addVoice(voiceId, voiceName, sampleBank.samples[voiceName])
+              }
 
-// ---------------------------------------------------------------- 
-// UI Handlers
-window.onload = function() {
+            // Create the new sequencer UI 
+              var ui = new SequencerUI(document.getElementById('sequencer'), sequencer, sequenceName, voices);
+
+  // Add listeners for save
+              ui.saveButton.addEventListener('click', function(event) {
+                  var toggleData = "";
+                  var voices = sequencer.getVoices(voiceIds);
+                  for(var i in voices) {
+                      var toggles = voices[i]['toggles']
+                      if(toggles) {
+                          for(var j in toggles) {
+                              toggleData += toggles[j];
+                          }
+                      }
+                  }
+
+                  socket.emit('sequence_data_update', {sequence_id : sequenceId, data : toggleData})
+              });
+
+              ui.aiToggle.addEventListener('click', function(event) {
+                  socket.emit('sequence_ai_update', {sequence_id : sequenceId, ai : ui.aiToggle.checked});
+              });
+
+  // Add listeners for name change
+              ui.nameInput.addEventListener('blur', function(event) {
+                  var name = event.target.value;
+                  if(name != '') {
+                      socket.emit('sequence_name_update', {sequence_id : sequenceId, name : event.target.value})
+                  }
+              });
+
+  // Add to list of sequencer uis
+              sequencerUIs.push(ui);
+          }, 
+          function error() {
+              console.log('Error loading sample bank');                         
+          }
+      ); 
+  });
+  */
+
+  // ---------------------------------------------------------------- 
+  // UI Handlers
   var instrumentSelect = document.getElementById('instrument-select');
   var createInstrumentButton = document.getElementById('create-instrument-btn');
 
@@ -155,53 +180,52 @@ window.onload = function() {
     }
   }
   updateInfo();
-}
-
+});
 // -----------------------------------------------------------
 // Data
 var instrument_data = [
-  {
-    "name" : "beatbox",
-    "type" : "percussion",
+    {
+      "name" : "beatbox",
+      "type" : "percussion",
+      "voices" : [
+        {"name" : "Kick", "file" : "./sound/Kick05-Longer.wav"}, 
+        {"name" : "Snare", "file" : "./sound/Snare04-Hi-Simmons1.wav"}, 
+        {"name" : "Hi-hat", "file" : "./sound/CH.WAV"}, 
+        {"name" : "Clap", "file" : "./sound/CP.WAV"}
+      ]
+    }, {
+    "name" : "saw-bass", 
+    "type" : "bass", 
     "voices" : [
-      {"name" : "Kick", "file" : "Kick05-Longer.wav"}, 
-      {"name" : "Snare", "file" : "Snare04-Hi-Simmons1.wav"}, 
-      {"name" : "Hi-hat", "file" : "CH.WAV"}, 
-      {"name" : "Clap", "file" : "CP.WAV"}
-    ]
-  }, {
-  "name" : "saw-bass", 
-  "type" : "bass", 
-  "voices" : [
-      {"name" : "C", "file" : "saw-bass-c.wav"},
-      {"name" : "C#","file" : "saw-bass-c-sharp.wav"},
-      {"name" : "D", "file" : "saw-bass-d.wav"},
-      {"name" : "D#","file" : "saw-bass-d-sharp.wav"},
-      {"name" : "E","file" : "saw-bass-e.wav"},{
-      "name" : "F","file" : "saw-bass-f.wav"},
-      {"name" : "F#","file" : "saw-bass-f-sharp.wav"},
-      {"name" : "G", "file" : "saw-bass-g.wav"},
-      {"name" : "G#", "file" : "saw-bass-g-sharp.wav"},
-      {"name" : "A", "file" : "saw-bass-a.wav"},
-      {"name" : "A#", "file" : "saw-bass-a-sharp.wav"},
-      {"name" : "B", "file" : "saw-bass-B.wav"}
-    ]
-  },{
-    "name" : "synth-flute", 
-    "type" : "flute", 
-    "voices" : [
-      {"name" : "C", "file" : "synth-flute-c.wav"},
-      {"name" : "C#", "file" : "synth-flute-c-sharp.wav"},
-      {"name" : "D", "file" : "synth-flute-d.wav"},
-      {"name" : "D#", "file" : "synth-flute-d-sharp.wav"},
-      {"name" : "E", "file" : "synth-flute-e.wav"},
-      {"name" : "F", "file" : "synth-flute-f.wav"},
-      {"name" : "F#", "file" : "synth-flute-f-sharp.wav"},
-      {"name" : "G","file" : "synth-flute-g.wav"},
-      {"name" : "G#","file" : "synth-flute-g-sharp.wav"},
-      {"name" : "A","file" : "synth-flute-a.wav"},
-      {"name" : "A#","file" : "synth-flute-a-sharp.wav"},
-      {"name" : "B", "file" : "synth-flute-B.wav"}
-    ]
-  }
-]
+        {"name" : "C", "file" : "./sound/saw-bass-c.wav"},
+        {"name" : "C#","file" : "./sound/saw-bass-c-sharp.wav"},
+        {"name" : "D", "file" : "./sound/saw-bass-d.wav"},
+        {"name" : "D#","file" : "./sound/saw-bass-d-sharp.wav"},
+        {"name" : "E","file" : "./sound/saw-bass-e.wav"},{
+        "name" : "F","file" : "./sound/saw-bass-f.wav"},
+        {"name" : "F#","file" : "./sound/saw-bass-f-sharp.wav"},
+        {"name" : "G", "file" : "./sound/saw-bass-g.wav"},
+        {"name" : "G#", "file" : "./sound/saw-bass-g-sharp.wav"},
+        {"name" : "A", "file" : "./sound/saw-bass-a.wav"},
+        {"name" : "A#", "file" : "./sound/saw-bass-a-sharp.wav"},
+        {"name" : "B", "file" : "./sound/saw-bass-B.wav"}
+      ]
+    },{
+      "name" : "synth-flute", 
+      "type" : "flute", 
+      "voices" : [
+        {"name" : "C", "file" : "./sound/synth-flute-c.wav"},
+        {"name" : "C#", "file" : "./sound/synth-flute-c-sharp.wav"},
+        {"name" : "D", "file" : "./sound/synth-flute-d.wav"},
+        {"name" : "D#", "file" : "./sound/synth-flute-d-sharp.wav"},
+        {"name" : "E", "file" : "./sound/synth-flute-e.wav"},
+        {"name" : "F", "file" : "./sound/synth-flute-f.wav"},
+        {"name" : "F#", "file" : "./sound/synth-flute-f-sharp.wav"},
+        {"name" : "G","file" : "./sound/synth-flute-g.wav"},
+        {"name" : "G#","file" : "./sound/synth-flute-g-sharp.wav"},
+        {"name" : "A","file" : "./sound/synth-flute-a.wav"},
+        {"name" : "A#","file" : "./sound/synth-flute-a-sharp.wav"},
+        {"name" : "B", "file" : "./sound/synth-flute-B.wav"}
+      ]
+    }
+  ];
